bdf6df7e56aaf6b78252748be26bb36805eb3e9a
Lilian <lilian.kojan@gmail.com>
2022-04-26 00:07:19 +0200

    started prep for OSF and submission

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-This repository contains the code for a partial least squares
-structural equation model examining predictore for climate
-protective behavior in a German sample.
+This is repository for the data examination and model estimation for the
+January-February 2021 study on predictors for behavioral change in the
+contexts of the climate crisis and the COVID-19 pandemic.
+
+This repository contains all materials to reproduce our analysis for the
+paper '??'.
+
+## Folder structure
+
+-   `_targets` contains the files for the targets pipeline
+    which enables a reproducible workflow.
+-   `data` contains all data required for this project and code to
+    handle data perparation, anonymization, and OSF up/downloads
+-   `docs` contains the website data.
+-   `figures` contains generated figures
+-   `renv` contains information for libraries used
+-   `R` contains scripts with helper functions
+-   `reports` contains the Rmd reports for the iterative
+    analysis and development of the models, as well as
+    additional analysis and survey information for use on
+    the website. The website is rendered from this folder.
+
+## Instructions for reproducibility
+
+This project uses `renv` for reproducibility. You can install the
+libraries we used by calling `renv::restore()` from the R terminal.
+
+The workflow up until model analysis uses `targets`.
--- _targets.R
+++ _targets.R
@@ -18,6 +18,10 @@ tar_option_set(packages = c("careless",
                             "psych",
                             "seminr",
                             "tidyverse"))
+# If you have the raw data
+# Uncomment and run the list below
+# Otherwise run this list
+
 
 list(
   tar_target(
@@ -34,7 +38,7 @@ list(
     raw_S2_file,
     "data/raw/S2-Raw-Data-20210208.csv",
     format = "file"
-    ),
+  ),
   tar_target(
     raw_incidence_20210112,
     "data/external/20210112-Fallzahlen.csv",
@@ -67,9 +71,24 @@ list(
   ),
   tar_target(
     survey_data_cc_sem,
-      survey_data_sem %>%
-        select(starts_with("CC")) %>%
+    survey_data_sem %>%
+      select(starts_with("CC")) %>%
       filter(!is.na(CCSKN))
+  ),
+  # code broken here
+  # to do:
+  # remove id and then
+  # make target that writes into "open" folder and can be uploaded to OSF
+  # so that it can be downloaded and used openly for sem recreation
+  # and in the new pipeline: leave off/comment out first part
+  # and repair OSF uplink/download
+  tar_target(
+    survey_data_cc_sem_open,
+    survey_data_sem %>%
+      select(starts_with("CC")) %>%
+      filter(!is.na(CCSKN)),
+    "data/open/survey_data_cc_sem.RDS",
+    format = "rds"
   ),
   tar_target(
     cc_model_1,
@@ -376,11 +395,364 @@ list(
     pls_predict(cc_model_2_d_2_proxy)
   )
 )
-# Data import
-# we should start with anonymized data
-# so the analysis can be fully reproduced with targets
-# data import
-# data cleaning
-# data upload?
-# model creation
-# model evaluation
+
+
+
+# Uncomment this if you start with RAW data
+# list(
+#   tar_target(
+#     raw_S1_file,
+#     "data/raw/S1-Raw-Data-20210118.csv",
+#     format = "file"
+#   ),
+#   tar_target(
+#     raw_S1_text_file,
+#     "data/raw/S1-data-choicetext.csv",
+#     format = "file"
+#   ),
+#   tar_target(
+#     raw_S2_file,
+#     "data/raw/S2-Raw-Data-20210208.csv",
+#     format = "file"
+#     ),
+#   tar_target(
+#     raw_incidence_20210112,
+#     "data/external/20210112-Fallzahlen.csv",
+#     format = "file"
+#   ),
+#   tar_target(
+#     raw_incidence_20210201,
+#     "data/external/20210201-Fallzahlen.csv",
+#     format = "file"
+#   ),
+#   tar_target(
+#     survey_data_cleaned,
+#     import_clean(raw_S1_file, raw_S2_file)
+#   ),
+#   tar_target(
+#     survey_data_incidence,
+#     include_incidence(survey_data_cleaned, raw_S1_text_file, raw_incidence_20210112, raw_incidence_20210201)
+#   ),
+#   tar_target(
+#     survey_data_param,
+#     param_clean(survey_data_cleaned)
+#   ),
+#   tar_target(
+#     survey_data_inc_param,
+#     param_clean(survey_data_incidence)
+#   ),
+#   tar_target(
+#     survey_data_sem,
+#     treat_missing(survey_data_param)
+#   ),
+#   tar_target(
+#     survey_data_cc_sem,
+#       survey_data_sem %>%
+#         select(starts_with("CC")) %>%
+#       filter(!is.na(CCSKN))
+#   ),
+#   tar_target(
+#     cc_model_1,
+#     estimate_cc_1(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_1_boot,
+#     bootstrap_model(cc_model_1, 5000)
+#   ),
+#   tar_target(
+#     cc_model_1_fs_boot,
+#     bootstrap_model(cc_model_1$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_1_ra,
+#     redundancy_cc(cc_model_1)
+#   ),
+#   tar_target(
+#     survey_data_addan,
+#     code_data_addan(survey_data_incidence, cc_model_1)
+#   ),
+#   tar_target(
+#     cc_model_2_a_1,
+#     estimate_cc_2_a_1(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_1_boot,
+#     bootstrap_model(cc_model_2_a_1, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_1_fs_boot,
+#     bootstrap_model(cc_model_2_a_1$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_1_ra,
+#     redundancy_cc(cc_model_2_a_1)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2,
+#     estimate_cc_2_a_2(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2_boot,
+#     bootstrap_model(cc_model_2_a_2, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2_fs_boot,
+#     bootstrap_model(cc_model_2_a_2$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2_ra,
+#     redundancy_cc(cc_model_2_a_2)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2_proxy,
+#     estimate_cc_2_a_2_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2_proxy_boot,
+#     bootstrap_model(cc_model_2_a_2_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_a_2_plspre,
+#     pls_predict(cc_model_2_a_2_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3,
+#     estimate_cc_2_a_3(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3_boot,
+#     bootstrap_model(cc_model_2_a_3, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3_fs_boot,
+#     bootstrap_model(cc_model_2_a_3$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3_ra,
+#     redundancy_cc(cc_model_2_a_3)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3_proxy,
+#     estimate_cc_2_a_3_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3_proxy_boot,
+#     bootstrap_model(cc_model_2_a_3_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_a_3_plspre,
+#     pls_predict(cc_model_2_a_3_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4,
+#     estimate_cc_2_a_4(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4_boot,
+#     bootstrap_model(cc_model_2_a_4, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4_fs_boot,
+#     bootstrap_model(cc_model_2_a_4$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4_ra,
+#     redundancy_cc(cc_model_2_a_4)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4_proxy,
+#     estimate_cc_2_a_4_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4_proxy_boot,
+#     bootstrap_model(cc_model_2_a_4_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_a_4_plspre,
+#     pls_predict(cc_model_2_a_4_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_b_1,
+#     estimate_cc_2_b_1(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_b_1_boot,
+#     bootstrap_model(cc_model_2_b_1, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_b_1_fs_boot,
+#     bootstrap_model(cc_model_2_b_1$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_b_1_ra,
+#     redundancy_cc(cc_model_2_b_1)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2,
+#     estimate_cc_2_b_2(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2_boot,
+#     bootstrap_model(cc_model_2_b_2, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2_fs_boot,
+#     bootstrap_model(cc_model_2_b_2$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2_ra,
+#     redundancy_cc(cc_model_2_b_2)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2_proxy,
+#     estimate_cc_2_b_2_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2_proxy_boot,
+#     bootstrap_model(cc_model_2_b_2_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_b_2_plspre,
+#     pls_predict(cc_model_2_b_2_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3,
+#     estimate_cc_2_b_3(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3_boot,
+#     bootstrap_model(cc_model_2_b_3, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3_fs_boot,
+#     bootstrap_model(cc_model_2_b_3$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3_ra,
+#     redundancy_cc(cc_model_2_b_3)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3_proxy,
+#     estimate_cc_2_b_3_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3_proxy_boot,
+#     bootstrap_model(cc_model_2_b_3_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_b_3_plspre,
+#     pls_predict(cc_model_2_b_3_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1,
+#     estimate_cc_2_c_1(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1_boot,
+#     bootstrap_model(cc_model_2_c_1, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1_fs_boot,
+#     bootstrap_model(cc_model_2_c_1$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1_ra,
+#     redundancy_cc(cc_model_2_c_1)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1_proxy,
+#     estimate_cc_2_c_1_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1_proxy_boot,
+#     bootstrap_model(cc_model_2_c_1_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_c_1_plspre,
+#     pls_predict(cc_model_2_c_1_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2,
+#     estimate_cc_2_c_2(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2_boot,
+#     bootstrap_model(cc_model_2_c_2, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2_fs_boot,
+#     bootstrap_model(cc_model_2_c_2$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2_ra,
+#     redundancy_cc(cc_model_2_c_2)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2_proxy,
+#     estimate_cc_2_c_2_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2_proxy_boot,
+#     bootstrap_model(cc_model_2_c_2_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_c_2_plspre,
+#     pls_predict(cc_model_2_c_2_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1,
+#     estimate_cc_2_d_1(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1_boot,
+#     bootstrap_model(cc_model_2_d_1, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1_fs_boot,
+#     bootstrap_model(cc_model_2_d_1$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1_ra,
+#     redundancy_cc(cc_model_2_d_1)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1_proxy,
+#     estimate_cc_2_d_1_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1_proxy_boot,
+#     bootstrap_model(cc_model_2_d_1_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_d_1_plspre,
+#     pls_predict(cc_model_2_d_1_proxy)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2,
+#     estimate_cc_2_d_2(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2_boot,
+#     bootstrap_model(cc_model_2_d_2, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2_fs_boot,
+#     bootstrap_model(cc_model_2_d_2$first_stage_model, 5000)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2_ra,
+#     redundancy_cc(cc_model_2_d_2)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2_proxy,
+#     estimate_cc_2_d_2_proxy(survey_data_cc_sem)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2_proxy_boot,
+#     bootstrap_model(cc_model_2_d_2_proxy, 500)
+#   ),
+#   tar_target(
+#     cc_model_2_d_2_plspre,
+#     pls_predict(cc_model_2_d_2_proxy)
+#   )
+# )
